e974691435848fa553973602d64978e2e546c111
Zhi Cheng Wu <zhiwu093@ed.amdsb.ca>
2019-10-10 12:21:48 -0400

    Add project files.

---
+++ Cash Machine/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Cash_Machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.tenderedBox = new System.Windows.Forms.TextBox();
+            this.printReceipt = new System.Windows.Forms.Button();
+            this.newOrderLabel = new System.Windows.Forms.Button();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.numberOfSprayBox = new System.Windows.Forms.NumericUpDown();
+            this.numberOfCureKitBox = new System.Windows.Forms.NumericUpDown();
+            this.numberOfWipesBox = new System.Windows.Forms.NumericUpDown();
+            this.labelchange = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfSprayBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfCureKitBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfWipesBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.ForeColor = System.Drawing.Color.Red;
+            this.calculateButton.Location = new System.Drawing.Point(146, 217);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(152, 34);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate Totals";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label
+            // 
+            this.label.ForeColor = System.Drawing.Color.Lime;
+            this.label.Location = new System.Drawing.Point(12, 68);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(151, 26);
+            this.label.TabIndex = 1;
+            this.label.Text = "Number of Spray";
+            // 
+            // Label2
+            // 
+            this.Label2.ForeColor = System.Drawing.Color.Lime;
+            this.Label2.Location = new System.Drawing.Point(15, 119);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(148, 32);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "Number of Cure Kit";
+            // 
+            // Label3
+            // 
+            this.Label3.ForeColor = System.Drawing.Color.Lime;
+            this.Label3.Location = new System.Drawing.Point(19, 164);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(144, 26);
+            this.Label3.TabIndex = 3;
+            this.Label3.Text = "Number of Wipes ";
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.ForeColor = System.Drawing.Color.Lime;
+            this.subTotalLabel.Location = new System.Drawing.Point(97, 254);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(310, 39);
+            this.subTotalLabel.TabIndex = 7;
+            this.subTotalLabel.Text = " Sub Total";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.ForeColor = System.Drawing.Color.Lime;
+            this.tenderedLabel.Location = new System.Drawing.Point(63, 392);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(100, 23);
+            this.tenderedLabel.TabIndex = 10;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.ForeColor = System.Drawing.Color.Lime;
+            this.changeLabel.Location = new System.Drawing.Point(165, 462);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(179, 51);
+            this.changeLabel.TabIndex = 11;
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateChangeButton.ForeColor = System.Drawing.Color.Red;
+            this.calculateChangeButton.Location = new System.Drawing.Point(146, 425);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(152, 34);
+            this.calculateChangeButton.TabIndex = 12;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            this.calculateChangeButton.Click += new System.EventHandler(this.CalculateChangeButton_Click);
+            // 
+            // tenderedBox
+            // 
+            this.tenderedBox.Location = new System.Drawing.Point(169, 389);
+            this.tenderedBox.Name = "tenderedBox";
+            this.tenderedBox.Size = new System.Drawing.Size(100, 26);
+            this.tenderedBox.TabIndex = 14;
+            // 
+            // printReceipt
+            // 
+            this.printReceipt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.printReceipt.ForeColor = System.Drawing.Color.Red;
+            this.printReceipt.Location = new System.Drawing.Point(146, 516);
+            this.printReceipt.Name = "printReceipt";
+            this.printReceipt.Size = new System.Drawing.Size(152, 39);
+            this.printReceipt.TabIndex = 15;
+            this.printReceipt.Text = "Print Receipt ";
+            this.printReceipt.UseVisualStyleBackColor = true;
+            this.printReceipt.Click += new System.EventHandler(this.PrintReceipt_Click);
+            // 
+            // newOrderLabel
+            // 
+            this.newOrderLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newOrderLabel.ForeColor = System.Drawing.Color.Red;
+            this.newOrderLabel.Location = new System.Drawing.Point(641, 516);
+            this.newOrderLabel.Name = "newOrderLabel";
+            this.newOrderLabel.Size = new System.Drawing.Size(129, 42);
+            this.newOrderLabel.TabIndex = 16;
+            this.newOrderLabel.Text = "New Order";
+            this.newOrderLabel.UseVisualStyleBackColor = true;
+            this.newOrderLabel.Click += new System.EventHandler(this.NewOrderLabel_Click);
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.ForeColor = System.Drawing.Color.Lime;
+            this.taxLabel.Location = new System.Drawing.Point(97, 293);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(234, 35);
+            this.taxLabel.TabIndex = 17;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.ForeColor = System.Drawing.Color.Lime;
+            this.totalLabel.Location = new System.Drawing.Point(97, 331);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(268, 61);
+            this.totalLabel.TabIndex = 18;
+            this.totalLabel.Text = "Total";
+            // 
+            // Title
+            // 
+            this.Title.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.Aqua;
+            this.Title.Location = new System.Drawing.Point(14, 13);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(473, 55);
+            this.Title.TabIndex = 19;
+            this.Title.Text = "CREP PROTECTION";
+            // 
+            // numberOfSprayBox
+            // 
+            this.numberOfSprayBox.Location = new System.Drawing.Point(169, 71);
+            this.numberOfSprayBox.Name = "numberOfSprayBox";
+            this.numberOfSprayBox.Size = new System.Drawing.Size(120, 26);
+            this.numberOfSprayBox.TabIndex = 20;
+            // 
+            // numberOfCureKitBox
+            // 
+            this.numberOfCureKitBox.Location = new System.Drawing.Point(169, 117);
+            this.numberOfCureKitBox.Name = "numberOfCureKitBox";
+            this.numberOfCureKitBox.Size = new System.Drawing.Size(120, 26);
+            this.numberOfCureKitBox.TabIndex = 21;
+            // 
+            // numberOfWipesBox
+            // 
+            this.numberOfWipesBox.Location = new System.Drawing.Point(169, 164);
+            this.numberOfWipesBox.Name = "numberOfWipesBox";
+            this.numberOfWipesBox.Size = new System.Drawing.Size(120, 26);
+            this.numberOfWipesBox.TabIndex = 22;
+            // 
+            // labelchange
+            // 
+            this.labelchange.ForeColor = System.Drawing.Color.Lime;
+            this.labelchange.Location = new System.Drawing.Point(63, 474);
+            this.labelchange.Name = "labelchange";
+            this.labelchange.Size = new System.Drawing.Size(100, 23);
+            this.labelchange.TabIndex = 23;
+            this.labelchange.Text = "Change";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1002, 589);
+            this.Controls.Add(this.labelchange);
+            this.Controls.Add(this.numberOfWipesBox);
+            this.Controls.Add(this.numberOfCureKitBox);
+            this.Controls.Add(this.numberOfSprayBox);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.newOrderLabel);
+            this.Controls.Add(this.printReceipt);
+            this.Controls.Add(this.tenderedBox);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.calculateButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cash Register";
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfSprayBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfCureKitBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfWipesBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.TextBox tenderedBox;
+        private System.Windows.Forms.Button printReceipt;
+        private System.Windows.Forms.Button newOrderLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.NumericUpDown numberOfSprayBox;
+        private System.Windows.Forms.NumericUpDown numberOfCureKitBox;
+        private System.Windows.Forms.NumericUpDown numberOfWipesBox;
+        private System.Windows.Forms.Label labelchange;
+    }
+}
+
---
+++ Cash Machine/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Cash_Machine
+{
+    public partial class Form1 : Form
+    {
+        const double SPRAY = 25.00;
+        const double CURE_KIT = 30.00;
+        const double WIPES = 18.00;
+        const double TAX = 0.13;
+        double subTotal, tax, total, tender, change;
+        int numberOfCurekit, numberOfSpray, numberOfWipes;
+
+        private void NewOrderLabel_Click(object sender, EventArgs e)
+        {
+            // object
+            Graphics g = this.CreateGraphics();
+
+            // resets the whole thing
+            g.Clear(Color.Black);
+            subTotalLabel.Text = "Subtotal:";
+            taxLabel.Text = "Tax:   ";
+            totalLabel.Text = "Total: ";
+            changeLabel.Text = "";
+            tenderedBox.Text = Convert.ToString("");
+            // clearing everything
+            numberOfCureKitBox.Value = 0;
+            numberOfSprayBox.Value = 0;
+            numberOfWipesBox.Value = 0;
+            subTotal = 0;
+            total = 0;
+            tender = 0;
+            change = 0;
+            tax = 0;
+            numberOfCurekit = 0;
+            numberOfSpray = 0;
+            numberOfWipes = 0;
+            costOfCurekit = 0;
+            costOfSpray = 0;
+            costOfWipes = 0;
+
+        }
+
+        private void PrintReceipt_Click(object sender, EventArgs e)
+        {
+            // objected created for the receipt 
+            Graphics g = this.CreateGraphics();
+            Font drawrceipt = new Font("Segoe Print", 10, FontStyle.Regular);
+            Font drawTitle = new Font("Arial", 15, FontStyle.Bold);
+            SolidBrush receiptBrush = new SolidBrush(Color.LimeGreen);
+            SolidBrush rectangleBrush = new SolidBrush(Color.White);
+            SolidBrush TitleBrush = new SolidBrush(Color.Teal);
+            Pen BorderPen = new Pen(Color.GhostWhite, 3);
+
+            // sound
+            SoundPlayer cashRegister = new SoundPlayer(Properties.Resources.cashRegister);
+            // background 
+            //g.FillRectangle(rectangleBrush, 330, 20, 300, 300);
+            Thread.Sleep(350);
+            g.DrawLine(BorderPen, 330, 20, 620, 20);
+            Thread.Sleep(350);
+            g.DrawLine(BorderPen, 330, 20, 330,320 );
+            Thread.Sleep(350);
+            g.DrawLine(BorderPen, 330, 320, 620, 320);
+            Thread.Sleep(350);
+            g.DrawLine(BorderPen, 620, 320, 620, 20);
+            //title
+            g.DrawString("Crep Protect", drawTitle, TitleBrush, 420, 30);
+            //made a random order generator
+            Random Order = new Random();
+            int ordernumber = Order.Next(1, 500);
+            //receipt 
+            cashRegister.Play();
+            g.DrawString("Tuesday October 8, 2019", drawrceipt, receiptBrush, 400, 60);
+            g.DrawString("Order Number:  " + ordernumber, drawrceipt, receiptBrush, 400, 75);
+            cashRegister.Play();
+            Thread.Sleep(350);
+            g.DrawString("001 Spray    x" + numberOfSpray, drawrceipt, receiptBrush, 350, 110);
+            g.DrawString("" + costOfSpray.ToString("C"), drawrceipt, receiptBrush, 500, 110);
+            cashRegister.Play();
+            Thread.Sleep(350);
+            g.DrawString("  002 CureKit   X" + numberOfCurekit, drawrceipt, receiptBrush, 340, 125);
+            g.DrawString("" + costOfCurekit.ToString("C"), drawrceipt, receiptBrush, 500, 125);
+            cashRegister.Play();
+            Thread.Sleep(350);
+            g.DrawString("003 Wipes   X" + numberOfWipes, drawrceipt, receiptBrush, 350, 140);
+            g.DrawString("" + costOfWipes.ToString("C"), drawrceipt, receiptBrush, 500, 140);
+            Thread.Sleep(350);
+            cashRegister.Play();
+            g.DrawString("Subtotal", drawrceipt, receiptBrush, 350, 165);
+            g.DrawString("" + subTotal.ToString("C"), drawrceipt, receiptBrush, 500, 165);
+            Thread.Sleep(350);
+            cashRegister.Play();
+            g.DrawString("Tax", drawrceipt, receiptBrush, 350, 180);
+            g.DrawString("" + tax.ToString("C"), drawrceipt, receiptBrush, 500, 180);
+            Thread.Sleep(350);
+            cashRegister.Play();
+            g.DrawString("Total", drawrceipt, receiptBrush, 350, 195);
+            g.DrawString("" + total.ToString("C"), drawrceipt, receiptBrush, 500, 195);
+            Thread.Sleep(350);
+            cashRegister.Play();
+            g.DrawString("Tendered", drawrceipt, receiptBrush, 350, 220);
+            g.DrawString("" + tender.ToString("C"), drawrceipt, receiptBrush, 500, 220);
+            Thread.Sleep(350);
+            cashRegister.Play();
+            g.DrawString("Change", drawrceipt, receiptBrush, 350, 235);
+            g.DrawString("" + change.ToString("C"), drawrceipt, receiptBrush, 500, 235) ;
+            Thread.Sleep(350);
+            cashRegister.Play();
+            g.DrawString("Have a nice day!", drawrceipt, receiptBrush, 430, 290);
+            cashRegister.Play();
+
+        }
+
+        double costOfSpray, costOfCurekit, costOfWipes;
+
+        private void CalculateChangeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numberOfCurekit = Convert.ToInt16(numberOfCureKitBox.Text);
+                numberOfSpray = Convert.ToInt16(numberOfSprayBox.Text);
+                numberOfWipes = Convert.ToInt16(numberOfWipesBox.Text);
+            }
+            catch
+            {
+                totalLabel.Text = "Please enter a numeric value";
+                return;
+            }
+            // calulation
+            costOfCurekit = CURE_KIT * numberOfCurekit;
+            costOfSpray = SPRAY * numberOfSpray;
+            costOfWipes = WIPES * numberOfWipes;
+            subTotal = costOfCurekit + costOfSpray + costOfWipes;
+            tax = subTotal * TAX;
+            total = subTotal + tax;
+
+            // showing the total, subtotal and tax on a label
+            subTotalLabel.Text = "Subtotal:   $" + subTotal;
+            taxLabel.Text = "Tax:          $" + tax;
+            totalLabel.Text = "Total:        $" + total;
+
+            // calculating change
+            try
+            {
+                tender = Convert.ToDouble(tenderedBox.Text);
+            }
+            catch
+            {
+                totalLabel.Text = "Please enter a numeric value";
+            }
+            change = tender - total;
+
+            changeLabel.Text = change.ToString("C");
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numberOfCurekit = Convert.ToInt16(numberOfCureKitBox.Text);
+                numberOfSpray = Convert.ToInt16(numberOfSprayBox.Text);
+                numberOfWipes = Convert.ToInt16(numberOfWipesBox.Text);
+            }
+            catch
+            {
+                totalLabel.Text = "Please enter a numeric value";
+                return;
+            }
+            // calulation
+            costOfCurekit = CURE_KIT * numberOfCurekit;
+            costOfSpray = SPRAY * numberOfSpray;
+            costOfWipes = WIPES * numberOfWipes;
+            subTotal = costOfCurekit + costOfSpray + costOfWipes;
+            tax = subTotal * TAX;
+            total = subTotal + tax;
+
+            // showing the total, subtotal and tax on a label
+            subTotalLabel.Text = "Subtotal:   " + subTotal.ToString("C");
+            taxLabel.Text = "Tax:          " + tax.ToString ("C");
+            totalLabel.Text = "Total:        " + total.ToString("C");
+        }
+
+    }
+}
